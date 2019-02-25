81b80989f1b77a299fc161c3dce1eb83640b8576
siltoon <siltoon1@gmail.com>
2019-02-25 16:06:42 +0100

    a

--- datasets.py
+++ datasets.py
@@ -4,8 +4,10 @@ import wget
 from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 
-#Generic load
-def load(url,train_size=0.8,seed=42):
+
+def load_bacteria(train_size=0.8,seed=42):
+    url = 'https://www.dropbox.com/s/conkqhwi5pd31yk/bacteria.npz?dl=1'
+
     filename = wget.download(url)
     data = np.load(filename)
     images = np.expand_dims(data['images']/255,axis=3)
@@ -14,9 +16,15 @@ def load(url,train_size=0.8,seed=42):
     test_size  = 1.-train_size
     X_train, X_test, y_train, y_test = train_test_split(images, labels, train_size = train_size, test_size=test_size, random_state=seed)
 
-    return (X_train,y_train),(X_test,y_test)
-
-def load_bacteria(train_size=0.8,seed=42):
-    print('Loading!!')
-    url = 'https://www.dropbox.com/s/conkqhwi5pd31yk/bacteria.npz?dl=1'
-    return load(url,train_size,seed)
+    print("Chargement du jeu de données réussi !\n")
+    print("Informations sur le jeu de données")
+    print("----------------------------------")
+    print("- Nombre d'images totales: ", images.shape[0])
+    print("- Proportion d'image dans le Training set:", train_size*100,"%")
+    print("- Nombre d'images dans le Training set:", X_train.shape[0])
+    print("- Nombre d'images dans le Validation set:", X_valid.shape[0])
+    print("\n")
+    print("Forme du tenseur d'entrée (input_shape): ", images.shape[1:])
+    print("Nombre de classes de sortie: ", labels.shape[1])
+    print("\n")
+    print("\n")
