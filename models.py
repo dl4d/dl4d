e849255a02edf45f7743002e3e6c708d6528dcb3
siltoon <siltoon1@gmail.com>
2019-02-25 16:50:11 +0100

    a

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from keras.utils import plot_model
 import matplotlib.pyplot as plt
 
-def plot_model(model):
+def plot_model_tree(model):
     plot_model(model, to_file='tmp_model.png')
     plt.figure(figsize=(20,20))
     plt.imshow(plt.imread("tmp_model.png"))
